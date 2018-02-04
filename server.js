45d6e65072c1a7e9ca67ac96e6f58d4812af4e39
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-02-04 09:32:28 -0500

    Update server

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+
+const DATA_FILE = path.join(__dirname, "data.json");
+
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+  next();
+});
+
+app.get("/api/hello", (req, res) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    res.setHeader("Cache-Control", "no-cache");
+    res.json(JSON.parse(data));
+  });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));

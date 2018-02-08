ee02b7e54502eac0535ed08a97ff3d998123507e
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-02-08 07:47:19 -0500

    Display Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,33 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Router, Route, Switch, HashRouter } from "react-router-dom";
+import { PropTypes } from "prop-types";
 
+var Routes = require("./components/Routes");
 var api = require("./utils/api");
 
 class App extends Component {
   state = {
     response: [],
-    loading: true
+    loading: true,
+    collapsed: true
   };
 
-  componentDidMount = () => {
-    api
-      .callApi()
-      .then(res => this.setState({ response: res, loading: false }))
-      .catch(err => console.log(err));
-  };
-
-  click = () => {
+  toggleHeader = () => {
+    this.setState(prevState => ({
+      collapsed: !prevState.collapsed
+    }));
     console.log(true)
   };
 
   render() {
-    const test = this.state.response.map(val => {
-      return <p key={val.id}> {val.name}</p>;
-    });
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        {this.state.loading ? <p>Loading...</p> : test}
-        <input placeholder="name" />
-        <br />
-        <input placeholder="text" />
-        <br />
-        <br />
-        <button onClick={this.click}>Submit</button>
+      <div>
+        <Routes
+          toggleHeader={this.toggleHeader}
+          collapsed={this.state.collapsed}
+        />
       </div>
     );
   }

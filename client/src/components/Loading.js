f08c1bc206825b01663bf334d41f0f56b7fd1fdd
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-02-17 12:03:59 -0500

    Add Loading component

---
+++ client/src/components/Loading.js
@@ -0,0 +1,14 @@
+var React = require("react");
+
+const Loading = props => {
+  return (
+    <div>
+      <center>
+        <hr />
+        <h1>Loading...</h1>
+      </center>
+    </div>
+  );
+};
+
+module.exports = Loading;

0bafd9945a1d51cc19f5272d12c32bad7f9cc2a6
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-02-17 12:50:29 -0500

    Update api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,18 @@
 module.exports = {
-  callApi() {
-    return fetch("/api/hello").then(response => response.json());
+  getCardData() {
+    return fetch("/api/cards").then(response => response.json());
+  },
+
+  addCard(card) {
+    return fetch("/api/add", {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        card
+      })
+    }).then(response => response.json());
   }
 };

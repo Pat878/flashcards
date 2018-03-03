16670d3e182e224522027ef8b380c237f7ab2807
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-03-03 08:46:28 -0500

    Update class to className

--- client/src/components/CardIndex.js
+++ client/src/components/CardIndex.js
@@ -7,9 +7,8 @@ const CardIndex = props => {
       <div className="col-md-4" key={card}>
         <div className="card mb-4 box-shadow">
           <div className="card-body">
-            <p className="card-text">
-              <h1>{props.startingCards}</h1>
-            </p>
+            <h1 className="card-text">{props.startingCards}</h1>
+
             <hr />
             <div className="d-flex justify-content-between align-items-center">
               <div className="btn-group">

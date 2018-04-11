c1858acba26bf367f94e8d5c456b50a17f2894a2
Patrick Mellon <mellon.patrick.j@gmail.com>
2018-04-11 19:12:21 -0400

    Add state to component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,80 +1,92 @@
 var React = require("react");
 //var PropTypes = require("prop-types");
 
-const Navbar = props => {
-  return (
-    <header>
-      <div className={props.collapsed ? "hidden" : "show"}>
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-8 col-md-7 py-4">
-              <h4 className="text-white">About</h4>
-              <p className="text-muted">
-                Add some information about the album below, the author, or any
-                other background context. Make it a few sentences long so folks
-                can pick up some informative tidbits. Then, link them off to
-                some social networking sites or contact information.
-              </p>
-            </div>
-            <div className="col-sm-4 offset-md-1 py-4">
-              <h4 className="text-white">Contact</h4>
-              <ul className="list-unstyled">
-                <li>
-                  <a href="" className="text-white">
-                    Follow on Twitter
-                  </a>
-                </li>
-                <li>
-                  <a href="" className="text-white">
-                    Like on Facebook
-                  </a>
-                </li>
-                <li>
-                  <a href="" className="text-white">
-                    Email me
-                  </a>
-                </li>
-              </ul>
+class Navbar extends React.Component {
+  state = {
+    collapsed: true
+  };
+
+  toggleHeader = () => {
+    this.setState(prevState => ({
+      collapsed: !prevState.collapsed
+    }));
+  };
+
+  render() {
+    return (
+      <header>
+        <div className={this.state.collapsed ? "hidden" : "show"}>
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-8 col-md-7 py-4">
+                <h4 className="text-white">About</h4>
+                <p className="text-muted">
+                  Add some information about the album below, the author, or any
+                  other background context. Make it a few sentences long so
+                  folks can pick up some informative tidbits. Then, link them
+                  off to some social networking sites or contact information.
+                </p>
+              </div>
+              <div className="col-sm-4 offset-md-1 py-4">
+                <h4 className="text-white">Contact</h4>
+                <ul className="list-unstyled">
+                  <li>
+                    <a href="" className="text-white">
+                      Follow on Twitter
+                    </a>
+                  </li>
+                  <li>
+                    <a href="" className="text-white">
+                      Like on Facebook
+                    </a>
+                  </li>
+                  <li>
+                    <a href="" className="text-white">
+                      Email me
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="navbar navbar-dark bg-dark box-shadow">
-        <div className="container d-flex justify-content-between">
-          <a href="" className="navbar-brand d-flex align-items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2"
+        <div className="navbar navbar-dark bg-dark box-shadow">
+          <div className="container d-flex justify-content-between">
+            <a href="" className="navbar-brand d-flex align-items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2"
+              >
+                <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
+                <circle cx="12" cy="13" r="4" />
+              </svg>
+              <strong>Flashcards</strong>
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarHeader"
+              aria-controls="navbarHeader"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              onClick={this.toggleHeader}
             >
-              <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
-              <circle cx="12" cy="13" r="4" />
-            </svg>
-            <strong>Flashcards</strong>
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarHeader"
-            aria-controls="navbarHeader"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            onClick={props.toggleHeader}
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
+              <span className="navbar-toggler-icon" />
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
-  );
-};
+      </header>
+    );
+  }
+}
 
 module.exports = Navbar;
